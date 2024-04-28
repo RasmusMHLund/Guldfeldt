e0b49a3abeff6cb4bedcf1afc1b24438788d3aa7
StrawbMilky <michellealsing00@gmail.com>
2024-04-28 18:53:01 +0200

    Added journeyman properties and constructor

--- Guldfeldt/Guldfeldt/Model/Journeyman.cs
+++ Guldfeldt/Guldfeldt/Model/Journeyman.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Guldfeldt.Model
 {
-    internal class Journeyman
-    {
+    public class Journeyman
+    {   
+        public string Name { get; set; }  
+        public DateTime DateOfBirth { get; set; }
+        public string SocialSecurityNumber { get; set; }
+        public string Email {  get; set; }
+        public int PhoneNumber { get; set; }
+        public bool MentorStatus { get; set; }
+        public int SalaryNumber { get; set; }
+
+        public Journeyman(string name, DateTime dateOfBirth, string socialSecurityNumber, string email, int phoneNumber, bool mentorStatus, int salaryNumber ) 
+        { 
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            SocialSecurityNumber = socialSecurityNumber;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            MentorStatus = mentorStatus;
+            SalaryNumber = salaryNumber;
+        }
     }
 }
