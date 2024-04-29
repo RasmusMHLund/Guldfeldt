8d720edc0dcc92206f5e5dde2d9853ce7d87194c
Patrick <patrickhass84@gmail.com>
2024-04-29 12:38:44 +0200

    small changes to wpf, added note class and repo

---
+++ Guldfeldt/Guldfeldt/Persistence/NoteRepo.cs
@@ -0,0 +1,26 @@
+﻿using Guldfeldt.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guldfeldt.Persistence
+{
+    public class NoteRepo
+    {
+        string? ConnectionString;
+
+        private List<Note> Notes;
+        public NoteRepo()
+        {
+            Notes = new List<Note>();
+
+            IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            string? ConnectionString = config.GetConnectionString("MyDBConnection");
+
+        }
+    }
+}
---
+++ Guldfeldt/SQLQuery1.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE APPRENTICE (
+SalaryNumber Int PRIMARY KEY,
+Name NVarChar(50),
+DateOfBirth DateTime2, 
+SocialSecurityNumber NVarChar(11),
+Email NVarChar(50),
+PhoneNumber Int
+);
+
+CREATE TABLE JOURNEYMAN (
+SalaryNumber Int PRIMARY KEY,
+Name NVarChar(50),
+DateOfBirth DateTime2, 
+SocialSecurityNumber NVarChar(11),
+Email NVarChar(50),
+PhoneNumber Int,
+MentorStatus Bit
+);
+
+CREATE TABLE WORKPLACE (
+WorkplaceId Int IDENTITY(1,1) PRIMARY KEY,
+Name NVarChar(50),
+Address NVarChar(200)
+);
